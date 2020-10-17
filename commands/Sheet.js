2c0c1ab9413a3fb08becbb40ed116cf01751f7ec
Prozilla <siebendebeule7@gmail.com>
2020-10-17 17:25:42 +0200

    sheet command

---
+++ commands/Sheet.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: "sheet",
+	description: "kali linux commands cheat-sheet",
+	aliases: ["cheatsheet", "cheat-sheet"],
+	usage: "!sheet",
+	execute(message, args) {
+		message.channel.send("This command is a WIP");
+	},
+};
